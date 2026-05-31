1c39471fa07b88527cce8b971a712339c99724e1
Byeone001 <halimqolbun@gmail.com>
2026-05-31 10:38:49 +0700

    Update

--- src/app/components/DashboardScreen.tsx
+++ src/app/components/DashboardScreen.tsx
@@ -17,7 +17,8 @@ export function DashboardScreen({ onStartAttendance, onOpenDrawer, userRole = 'g
 
   // Koordinat Sekolah (Sesuai FaceRecognitionScreen)
   //const SCHOOL_LOCATION = { lat: -7.693503, lng: 111.333048 };
-  const SCHOOL_LOCATION = { lat: -7.693499, lng: 111.333072 };
+  //const SCHOOL_LOCATION = { lat: -7.693499, lng: 111.333072 };
+  const SCHOOL_LOCATION = { lat: -7.573549, lng: 111.442077 };
   const ALLOWED_RADIUS = 100; // Meter (sesuai SRS FR-02)
 
   const [schedules, setSchedules] = useState<any[]>([]);
